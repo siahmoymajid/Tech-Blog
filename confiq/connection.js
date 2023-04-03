531919e64f6f8a43f95427865c1d3ea5bafd4f94
siahmoymajid <122416542+siahmoymajid@users.noreply.github.com>
2023-04-03 13:17:29 -0700

    updated file

--- confiq/connection.js
+++ confiq/connection.js
@@ -1,15 +1,18 @@
-require("dotenv").config();
 const Sequelize = require("sequelize");
-
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.enc)
-  : new Sequelize(, {
+require("dotenv").config();
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
       host: "localhost",
       dialect: "mysql",
-      port: 3001;
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
-
+      port: 3306,
+    }
+  );
+}
 module.exports = sequelize;
