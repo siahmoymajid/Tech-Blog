f55fff65cd3836383e1f0a21ea94df873ddb6ab8
siahmoymajid <122416542+siahmoymajid@users.noreply.github.com>
2023-04-03 22:01:32 -0700

    fixed the bug on index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
-const homeRoutes = require("./homeroutes.js");
-const dashboardRoutes = require("../dashboardroutes.js");
+const homeRoutes = require("./homeroute.js");
+const dashboardRoute = require("./dashboardroutes.js");
 
-router.use("/", homeRoutes);
-router.use("/dashboard", dashboardRoutes);
-router.use("/api", apiRoutes);
+router.use("/", homeRoute);
+router.use("/dashboard", dashboardRoute);
+router.use("/api", apiRoute);
+
+
+module.exports = router;
